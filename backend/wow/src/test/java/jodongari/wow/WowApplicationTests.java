44999e469e8c915e3ef4b12eed291c4846b04b08
changwoo <qaqa313a@gmail.com>
2021-12-05 15:10:24 +0900

    initial commit

---
+++ backend/wow/src/test/java/jodongari/wow/WowApplicationTests.java
@@ -0,0 +1,13 @@
+package jodongari.wow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
