e83dca108ba926d63df529680e77ebbcfe7c65a3
changwoo <qaqa313a@gmail.com>
2021-12-12 17:33:46 +0900

    WOW-9 Update gradle and Write Video, BaseDateTime entity

---
+++ backend/wow/src/main/java/jodongari/wow/video/repository/VideoEntity.java
@@ -0,0 +1,64 @@
+package jodongari.wow.video.repository;
+
+import jodongari.wow.common.BaseDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Getter
+@Entity
+@Table(name="VIDEO")
+@NoArgsConstructor
+public class VideoEntity extends BaseDateTime {
+
+    @Id
+    @Column(name="VIDEO_HASH", nullable = false)
+    @Size(max = 32)
+    private String videoHash;
+
+    @Column(name="VIDEO_NM", nullable = false)
+    @Size(max = 100)
+    private String videoName;
+
+    @Column(name="MANIFEST_PATH", nullable = false)
+    @Size(max = 200)
+    private String manifestPath;
+
+    @Column(name = "VIDEO_DESCRIPTION")
+    @Size(max = 500)
+    private String description;
+
+    @Column(name = "VIDEO_DELETE_FLAG",
+            columnDefinition = "boolean default false")
+    private Boolean deleteFlag;
+
+    @Column(name = "VIDEO_LIKE_COUNT",
+            columnDefinition = "int default 0")
+    private Long likeCount;
+
+    @Column(name = "VIDEO_DISLIKE_COUNT",
+            columnDefinition = "int default 0")
+    private Long dislikeCount;
+
+    @Column(name = "VIDEO_RUNNING_TIME", nullable = false)
+    private Long runningTime;
+
+    @Builder
+    public VideoEntity(String videoHash, String videoName, String manifestPath, String description, Boolean deleteFlag,
+                       Long likeCount, Long dislikeCount, Long runningTime) {
+        this.videoHash = videoHash;
+        this.videoName = videoName;
+        this.manifestPath = manifestPath;
+        this.description = description;
+        this.deleteFlag = deleteFlag;
+        this.likeCount = likeCount;
+        this.dislikeCount = dislikeCount;
+        this.runningTime = runningTime;
+    }
+}
