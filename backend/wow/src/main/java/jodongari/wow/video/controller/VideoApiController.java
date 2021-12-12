31cd3b607b60fe1f17ebd930b4239ad5072e8db5
정지수 <kia0097@naver.com>
2021-12-12 17:33:48 +0900

    Delete consume

--- backend/wow/src/main/java/jodongari/wow/video/controller/VideoApiController.java
+++ backend/wow/src/main/java/jodongari/wow/video/controller/VideoApiController.java
@@ -5,7 +5,6 @@ import jodongari.wow.video.dto.request.VideoUploadRequest;
 import jodongari.wow.video.dto.response.VideoUploadResponse;
 import jodongari.wow.video.service.VideoApiService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,12 +16,7 @@ public class VideoApiController {
 
     private final VideoApiService videoApiService;
 
-    @PostMapping(
-            value = VideoApiUrl.VIDEO_UPLOAD,
-            consumes = {
-                    MediaType.MULTIPART_FORM_DATA_VALUE,
-                    MediaType.APPLICATION_OCTET_STREAM_VALUE}
-    )
+    @PostMapping(VideoApiUrl.VIDEO_UPLOAD)
     public ResponseEntity<VideoUploadResponse> upload(@RequestBody VideoUploadRequest request) {
         return videoApiService.upload(request);
     }
