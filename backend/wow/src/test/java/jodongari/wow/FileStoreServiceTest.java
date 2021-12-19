ca1e058a2913fc32d7aae7862b4c8f694b6cea3d
조만기 <macho@ebay.com>
2021-12-19 16:56:03 +0900

    WOW-17 Implement Video File Upload Service

---
+++ backend/wow/src/test/java/jodongari/wow/FileStoreServiceTest.java
@@ -0,0 +1,19 @@
+package jodongari.wow;
+
+import jodongari.wow.video.service.FileStoreService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FileStoreServiceTest {
+
+    @Autowired
+    FileStoreService fileStoreService;
+
+    @Test
+    public void fileSaveTest(){
+
+    }
+
+}
