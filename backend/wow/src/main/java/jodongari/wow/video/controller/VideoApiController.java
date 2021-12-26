9eac48fee15af751fa66c7a01ab42b32b2914983
조만기 <macho@ebay.com>
2021-12-26 16:25:49 +0900

    WOW-26 video upload controller error fix

--- backend/wow/src/main/java/jodongari/wow/video/controller/VideoApiController.java
+++ backend/wow/src/main/java/jodongari/wow/video/controller/VideoApiController.java
@@ -5,19 +5,24 @@ import jodongari.wow.video.dto.request.VideoUploadRequest;
 import jodongari.wow.video.dto.response.VideoUploadResponse;
 import jodongari.wow.video.service.VideoApiService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
-@RestController("/api/video")
+@RestController
+@RequestMapping("/api/video")
 public class VideoApiController {
 
     private final VideoApiService videoApiService;
 
-    @PostMapping(VideoApiUrl.VIDEO_UPLOAD)
-    public ResponseEntity<VideoUploadResponse> upload(@RequestBody VideoUploadRequest request) {
+    @PostMapping(value = VideoApiUrl.VIDEO_UPLOAD,
+    consumes = {
+            MediaType.MULTIPART_FORM_DATA_VALUE,
+            MediaType.APPLICATION_OCTET_STREAM_VALUE
+    })
+    public ResponseEntity<VideoUploadResponse> upload(@ModelAttribute VideoUploadRequest request) {
         return videoApiService.upload(request);
     }
 }
