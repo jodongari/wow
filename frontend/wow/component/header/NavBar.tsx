e9afec300e85ec346e282456bb2134264dd7de99
김재현 <kimjaehyun9409@gmail.com>
2022-01-02 13:47:36 +0900

    WOW-33 Menu activate animation

--- frontend/wow/component/header/NavBar.tsx
+++ frontend/wow/component/header/NavBar.tsx
@@ -1,23 +1,38 @@
 import {Menu, Segment} from 'semantic-ui-react'
 import {NextPage} from "next";
+import Link from "next/link";
+import React, {useState} from "react";
 
 const NavBar: NextPage = () => {
 
-    const activeItem: string = 'home';
+    const [activeItem, setActiveItem] = useState('home');
+
+    const changeActiveItem = (menuItem: string) => {
+       setActiveItem(menuItem)
+    }
+
     return (
         <Segment inverted>
             <Menu inverted pointing secondary>
+                <Link href="/">
                 <Menu.Item
                     name='home'
                     active={activeItem === 'home'}
-                />
+                    onClick={() => changeActiveItem('home')}
+                >
+                </Menu.Item>
+                </Link>
+                <Link href="/MyPage">
                 <Menu.Item
                     name='messages'
                     active={activeItem === 'messages'}
+                    onClick={() => changeActiveItem('messages')}
                 />
+                </Link>
                 <Menu.Item
                     name='friends'
                     active={activeItem === 'friends'}
+                    onClick={() => changeActiveItem('friends')}
                 />
             </Menu>
         </Segment>
