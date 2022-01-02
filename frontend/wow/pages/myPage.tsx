b9aea91f23fbc127347bded2d8b217de9114b80c
김재현 <kimjaehyun9409@gmail.com>
2022-01-02 16:32:25 +0900

    Add test files of a mypage and a upload Page

---
+++ frontend/wow/pages/myPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Router Test 용으로 이후 MyPage 를 구현할 예정입니다.
+const MyPage = () => {
+    return (
+        <div>
+            This is MyPage
+        </div>
+    );
+};
+
+export default MyPage;
