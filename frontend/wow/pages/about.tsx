c0d629073ece03332926fd157462e7e38c00c9c9
김재현 <kimjaehyun9409@gmail.com>
2022-01-02 15:35:56 +0900

    feature/WOW-36 implementation move animation of menu

---
+++ frontend/wow/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>This is Test Menu Page</h1>
+        </div>
+    );
+};
+
+export default About;
--- frontend/wow/pages/index.tsx
+++ frontend/wow/pages/index.tsx
@@ -1,19 +1,37 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import Head from 'next/head'
 import 'semantic-ui-css/semantic.min.css'
-import Layout from "../component/Layout";
 import styles from "../styles/Home.module.css"
+import VideoList from "../component/video/VideoList";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.header}>
-        <Head>
-            <title>WoW-tube</title>
-            <meta name="description" content="Generate by create next app" />
-        </Head>
-        <Layout/>
-    </div>
-  )
+
+    const [list, setList] = useState<any>([]);
+    const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+    const getDate = () => {
+        axios.get(API_URL).then((res) => {
+            console.log(res.data);
+            setList(res.data);
+        })
+    }
+
+    useEffect(() => {
+        getDate();
+    }, []);
+
+    return (
+        <div className={styles.header}>
+            <Head>
+                <title>WoW-tube</title>
+                <meta name="description" content="Generate by create next app"/>
+            </Head>
+            <div>
+                <VideoList list={list}/>
+            </div>
+        </div>
+    )
 }
 
 export default Home
