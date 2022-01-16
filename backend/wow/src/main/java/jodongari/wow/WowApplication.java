d639c4186dc81b8cdb8b92dfe9c10ec8d66537de
GGoMak <kia0097@naver.com>
2022-01-16 15:33:47 +0900

    test

--- backend/wow/src/main/java/jodongari/wow/WowApplication.java
+++ backend/wow/src/main/java/jodongari/wow/WowApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WowApplication {
 
+	// test
 	public static void main(String[] args) {
 		SpringApplication.run(WowApplication.class, args);
 	}
