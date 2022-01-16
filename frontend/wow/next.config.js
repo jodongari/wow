093beaf16b75156654d4da09bb70839d8c65d1ea
CHANGWOO JUNG <59639001+ChangWoo95@users.noreply.github.com>
2022-01-16 14:29:45 +0900

    WOW-40 Implement http-proxy-middleware (#29)

--- frontend/wow/next.config.js
+++ frontend/wow/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.BACKEND_API_SERVER}:path*`,
+      },
+    ]
+  },
 }
