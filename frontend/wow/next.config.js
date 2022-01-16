f1c39d75e32a1b313cea6cfbe0c51ea20fe3d69b
김재현 <kimjaehyun9409@gmail.com>
2022-01-16 17:21:36 +0900

    test

--- frontend/wow/next.config.js
+++ frontend/wow/next.config.js
@@ -5,7 +5,9 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/:path*',
+        // source: /api/~ 로 시작되는 모든 API를 destination(localhost:8080/~) 으로 전달
+        // 추후 'api'를 API 컨벤션 회의를 통해 변경해야 합니다.
+        source: '/api/:path*',
         destination: `${process.env.BACKEND_API_SERVER}:path*`,
       },
     ]
