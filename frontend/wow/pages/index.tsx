b94ca8dd4ab099923a15a2fd14d5dab45ed0d79b
김재현 <kimjaehyun9409@gmail.com>
2022-01-16 13:31:02 +0900

    Change the index file from dynamic to static

--- frontend/wow/pages/index.tsx
+++ frontend/wow/pages/index.tsx
@@ -3,25 +3,9 @@ import Head from 'next/head'
 import 'semantic-ui-css/semantic.min.css'
 import styles from "../styles/Home.module.css"
 import VideoList from "../component/video/VideoList";
-import React, {useEffect, useState} from "react";
 import axios from "axios";
 
-const Home: NextPage = () => {
-
-    // 영상 List 를 요청할 URL
-    const API_VIDEO_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
-    const [list, setList] = useState<any>([]);
-
-    const getDate = () => {
-        axios.get(API_VIDEO_URL).then((res) => {
-            setList(res.data);
-        })
-    }
-
-    useEffect(() => {
-        getDate();
-    }, []);
-
+const Home: NextPage = ({list}: any) => {
     return (
         <div className={styles.header}>
             <Head>
@@ -35,4 +19,17 @@ const Home: NextPage = () => {
     )
 }
 
+export async function getStaticProps(){
+    const apiUrl: any = process.env.apiUrl;
+    const res = await axios.get(apiUrl);
+    const data = res.data;
+
+    return {
+        props: {
+            list: data,
+            name: process.env.name,
+        },
+    };
+}
 export default Home
+
