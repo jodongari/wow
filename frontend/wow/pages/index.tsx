476ce859bb8f9aad16204c9cedfe3102c37125f1
김재현 <kimjaehyun9409@gmail.com>
2022-01-23 13:12:33 +0900

    WOW-48 Split the best and recent Video section

--- frontend/wow/pages/index.tsx
+++ frontend/wow/pages/index.tsx
@@ -4,6 +4,7 @@ import 'semantic-ui-css/semantic.min.css'
 import styles from "../styles/Home.module.css"
 import VideoList from "../component/video/VideoList";
 import axios from "axios";
+import {Divider, Header} from 'semantic-ui-react';
 
 const Home: NextPage = ({list}: any) => {
     return (
@@ -13,7 +14,16 @@ const Home: NextPage = ({list}: any) => {
                 <meta name="description" content="Generate by create next app"/>
             </Head>
             <div>
-                <VideoList list={list}/>
+                <Header as="h3" style={{paddingTop: 40}}>
+                    베스트 영상
+                </Header>
+                <Divider/>
+                <VideoList list={list.slice(0, 9)}/>
+                <Header as="h3" style={{paddingTop: 40}}>
+                    최신 영상
+                </Header>
+                <Divider/>
+                <VideoList list={list.slice(9)}/>
             </div>
         </div>
     )
