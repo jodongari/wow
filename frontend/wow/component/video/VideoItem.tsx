86a5c913888783742058b9b2825f9fdf87495480
김재현 <kimjaehyun9409@gmail.com>
2022-01-23 15:41:17 +0900

    Refactoring by using getServerSideProps()

--- frontend/wow/component/video/VideoItem.tsx
+++ frontend/wow/component/video/VideoItem.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Button, Header} from "semantic-ui-react";
 import styles from "../../styles/Item.module.css"
+import {GetServerSideProps} from "next";
+import axios from "axios";
 
-// 추후 any 를 VideoItem 타입으로 변경 예정
-const VideoItem = ({item}: any) => {
+const VideoItem = ({item}: { item: any }) => {
     const {
         name,
         image_link,
@@ -37,15 +38,3 @@ const VideoItem = ({item}: any) => {
 };
 
 export default VideoItem;
-
-// 추후 Video 정보에 대한 interface (type)을 만들어 정적 type화를 진행할 예정
-interface VideoItem {
-    name: string;
-    image_link: string;
-    price: number;
-    description: string;
-    updated_at: string;
-    category: string;
-    product_type: string;
-    product_link: string;
-}
