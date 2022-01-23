5127f7a0e38d9b95c4cb878687aca70d1f790c18
김재현 <kimjaehyun9409@gmail.com>
2022-01-23 16:38:49 +0900

    WOW-53 Test dash.js

--- frontend/wow/pages/about.tsx
+++ frontend/wow/pages/about.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Head from 'next/head';
 
 const About = () => {
     return (
         <div>
             <h1>This is Test Menu Page</h1>
+            <div>
+                <Head>
+                    <script async src="http://cdn.dashjs.org/v3.1.0/dash.all.min.js"></script>
+                </Head>
+                <video data-dashjs-player autoPlay src="~/api/video/VIDEO_DOWNLOAD"
+                       controls>
+                </video>
+                <video data-dashjs-player autoPlay src="https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd"
+                       controls>
+                </video>
+            </div>
         </div>
     );
 };
