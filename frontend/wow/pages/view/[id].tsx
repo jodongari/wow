482853fda6ddc62786c18d0d48da8d445ac0cf7c
김재현 <kimjaehyun9409@gmail.com>
2022-01-23 15:41:17 +0900

    Refactoring by using getServerSideProps()

--- frontend/wow/pages/view/[id].tsx
+++ frontend/wow/pages/view/[id].tsx
@@ -2,28 +2,28 @@ import React, {useEffect, useState} from 'react';
 import {useRouter} from "next/router";
 import Axios from 'axios';
 import VideoItem from "../../component/video/VideoItem";
+import {GetServerSideProps} from "next";
+import axios from "axios";
 
-const Post = () => {
-    const router = useRouter();
-    const {id} = router.query;
-
-    const [item, setItem] = useState({});
-    const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`
-
-    const getData = () => {
-        Axios.get(API_URL)
-            .then((res: any) => {
-                setItem(res.data);
-            })
-    }
-
-    useEffect(()=>{
-        getData();
-    }, [id]);
+const Post = ({item}: any) => {
 
     return (
-        <VideoItem item={item}/>
+        <VideoItem item ={item}/>
     );
 };
 
 export default Post;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const id = context.params?.id; // ?: null 체크
+    const apiUrl: string = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+    const res = await axios.get(apiUrl);
+    const data = res.data;
+
+    return {
+        props: {
+            item: data,
+            name: process.env.name,
+        },
+    }
+}
