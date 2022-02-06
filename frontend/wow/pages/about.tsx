eb2cf524bedec4af8c6745b44bf68f9c6d3cc56c
jhkim-ai <78546091+jhkim-ai@users.noreply.github.com>
2022-02-06 16:12:18 +0900

    WOW-62 Add Header request of Video API (#46)

--- frontend/wow/pages/about.tsx
+++ frontend/wow/pages/about.tsx
@@ -1,19 +1,74 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import Head from 'next/head';
+import Dashjs from "dashjs";
+import {GetServerSideProps, GetStaticProps} from "next";
+import axios from "axios";
+
+
 
 const About = () => {
+    let player;
+    init();
+
     return (
         <>
-            <Head>
-                <script async src="http://cdn.dashjs.org/v3.1.0/dash.all.min.js"></script>
-            </Head>
+            {/*<Head>*/}
+            {/*    <script async src="http://cdn.dashjs.org/v3.1.0/dash.all.min.js"></script>*/}
+            {/*</Head>*/}
             <div>
-                <video data-dashjs-player autoPlay src="http://localhost:3000/api/video/v1/videoDownload/mpddash.mpd"
-                       controls>
-                </video>
+                {/* eslint-disable-next-line react/no-string-refs */}
+                <video controls={true}/>
             </div>
         </>
     );
 };
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    init();
+
+    // 실제 서비스 로직
+    /*
+    data.apiUrl = apiUrl;
+     */
+
+    return {
+        props: {
+            name: process.env.name,
+        },
+    }
+}
+
+const init = () => {
+    let video,
+        player,
+        url = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
+
+    video = document.querySelector("video");
+    console.log(video);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    player = Dashjs.MediaPlayer().create();
+
+    /* Extend RequestModifier class and implement our own behaviour */
+    // @ts-ignore
+    player.extend("RequestModifier", () => {
+        return {
+            modifyRequestHeader: function (xhr: any) {
+                /* Add custom header. Requires to set up Access-Control-Allow-Headers in your */
+                /* response header in the server side. Reference: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/setRequestHeader */
+                // xhr.setRequestHeader('X-VIDEO-HASH', '');
+                return xhr;
+            },
+            modifyRequestURL: (url: any) => {
+                /* Modify url adding a custom query string parameter */
+                return url + '?customQuery=value';
+            }
+        };
+    });
+
+    // @ts-ignore
+    player.initialize(video, url, true);
+}
+
+
 
 export default About;
