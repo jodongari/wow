4e38cf32c49b3cf9549ea60ba3b179d49f2cd485
김재현 <kimjaehyun9409@gmail.com>
2022-02-13 15:33:42 +0900

    WOW-66 Implement a Video Upload Page

--- frontend/wow/pages/upload.tsx
+++ frontend/wow/pages/upload.tsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Button} from "semantic-ui-react";
+import axios from "axios";
 
 // Router Test 용으로 이후 Upload Page 를 구현할 예정입니다.
 const Upload = () => {
+
+    const [videoFile, setVideoFile] = useState([]);
+    const [videoName, setVideoName] = useState("");
+    const [description, setDescription] = useState("");
+
+    let video: any;
+
+    const onFileUploadHandler = (e: any) => {
+
+        setVideoFile(e.target.files[0]);
+        setVideoName(e.target.files[0].name);
+        video = e.target.files[0]
+        console.log(e.target.files[0]);
+        console.log("나와라 비디오");
+        console.log(videoFile);
+        console.log(video);
+
+        console.log(e.target.files[0].name);
+        // setVideoName(videoFile.name);
+    }
+
+    const onClickUploadListener = (e: any) => {
+        console.log("업로드 버튼을 클릭했습니다");
+        const fd = new FormData();
+        fd.append("video", video);
+        fd.append("videoName", videoName);
+        fd.append("description", "sample description");
+
+        axios.post('http://localhost:8080/board/WriteBoard.do', fd, {
+            headers: {
+                        "Content-Type": `multipart/form-data; `,
+                     },
+        }).then(res => {
+            console.log(res);
+        })
+    }
+
     return (
         <div>
-            This is Upload Page
+            <input type={"file"} id={"videoFile"} onChange={onFileUploadHandler}/>
+            <Button onClick={onClickUploadListener}>업로드</Button>
         </div>
     );
 };
